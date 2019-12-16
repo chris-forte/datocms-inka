f96a8c35bb14395358556d7ba4c67816243f2ac9
Chris Forte <chrisforte@Chriss-iMac-9.local>
2019-12-16 09:56:49 +0000

    updated local site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
+/*
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`
 });
-
+*/
+require("dotenv").config();
 module.exports = {
   siteMetadata: {
     title: `Creative Portfolio`
